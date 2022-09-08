b8fd57aa2d86a2b0a4fe008a4b0e1069c578aa6c
kye <kye-a@hotmail.com>
2022-09-08 17:17:33 +1000

    Added new MEthods to PlayerUtils: GetUserFromClient (Accepts IChatClient) GertPlayerFromClient (Accepts IChatClient)

--- Eco.EM.Framework/Utils/PlayerUtils.cs
+++ Eco.EM.Framework/Utils/PlayerUtils.cs
@@ -1,4 +1,5 @@
 ﻿using Eco.Gameplay.Players;
+using Eco.Gameplay.Systems.Chat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,8 @@ namespace Eco.EM.Framework.Utils
         public static User GetUserByName(string UserName) => UserManager.FindUserByName(UserName);
         public static Player GetPlayerByName(string PlayerName) => GetUserByName(PlayerName).Player;
         public static User GetUser(string Filter) => UserManager.Users.FirstOrDefault(u => u.Name == Filter || u.SlgId == Filter || u.SteamId == Filter);
-
+        public static User GetUserFromClient(IChatClient client) => GetUser(client.Name) is not null ? GetUser(client.Name) : null;
+        public static Player GetPlayerFromClient(IChatClient client) => GetPlayerByName(client.Name) is not null ? GetPlayerByName(client.Name) : null;
         public static string WhoAmI(User user)
         {
             if (!string.IsNullOrWhiteSpace(user.SteamId))
