0dbf9d0e4db5bd9527d1924e26c1330afeea9a52
kye <kye-a@hotmail.com>
2022-09-22 15:38:14 +1000

    Added new Extensions for Item and Inventory for Comparison and inventory searching

---
+++ Eco.EM.Framework/Extentsions/Items/Item.cs
@@ -0,0 +1,16 @@
+﻿using Eco.Gameplay.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eco.EM.Framework.Extentsions.Items
+{
+    public static partial class @Item
+    {
+#nullable enable
+        public static bool ItemsEqual(this Gameplay.Items.Item x, Gameplay.Items.Item y) => x.DisplayName.NotTranslated.Equals(y.DisplayName.NotTranslated);
+#nullable disable
+    }
+}
